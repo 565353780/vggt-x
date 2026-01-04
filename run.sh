d002c7b80d1bee8fb0dfc16bb0cf7bf8eb25ef34
chLi <565353780@qq.com>
2026-01-04 15:47:29 +0800

    add libcuda path

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python demo_colmap.py \
+LIBRARY_PATH=/usr/local/cuda-12.4/targets/x86_64-linux/lib/stubs:$LIBRARY_PATH python demo_colmap.py \
   --scene_dir $HOME/chLi/Dataset/GS/haizei_1/input_10views/ \
   --shared_camera \
   --use_ga
